ae130bfd56b84bde27ef7c0a1ea22d1c0cbc094d
Luis Esteban <rodriguezjluis0@gmail.com>
2020-10-22 23:50:23 -0500

    fix

--- Unidad_3/Ejemplos/eje3_11.c
+++ Unidad_3/Ejemplos/eje3_11.c
@@ -3,7 +3,7 @@
 int main() {
     char c;
     c = 97; /* el valor en decimal del codigo ASCII */
-    print("%d\n", c);
+    printf("%d\n", c);
     c = 'a'; /* el carcter entre comillas */
     printf("%c\n", c);
     c = 0x61; /* el valor en hexadecimal del codigo ASCII */
